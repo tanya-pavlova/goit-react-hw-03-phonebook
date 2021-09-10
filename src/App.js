6fc6f56be70513046066c57181422116039b8617
tanya-pavlova <70815370+tanya-pavlova@users.noreply.github.com>
2021-09-10 17:25:02 +0300

    hw3

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import { Component } from "react";
+import styles from "./app.module.css";
+import { v4 as uuidv4 } from "uuid";
+import Contacts from "./components/Contacts";
+import Form from "./components/Form";
+import Filter from "./components/Filter";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    contacts.find((contact) => contact.name === name)
+      ? alert(`${name} is already exist!! Try one more time, please!`)
+      : contacts.find((contact) => contact.number === number)
+      ? alert(`${number} is already exist!! Try one more time, please!`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  filteredContacts() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+
+  onFilterHandleChange = (filter) => {
+    this.setState({ filter });
+  };
+
+  render() {
+    const visibleContacts = this.filteredContacts();
+    const { filter } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+
+        <Form addContact={this.addContact} />
+
+        <h2 className={styles.title}>Contacts</h2>
+
+        <Filter
+          filter={filter}
+          onFilterHandleChange={this.onFilterHandleChange}
+        />
+
+        <Contacts
+          contact={visibleContacts}
+          ondeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,24 @@
+
+import styles from './filter.module.css'
+
+const Filter = ({ filter, onFilterHandleChange }) => {
+    const onHandleChange = event => {
+        onFilterHandleChange(event.target.value);
+    };
+    return (
+        <div className={styles.container} >
+            <label className={styles.label}>
+                Find contacts by name
+                <input
+                    type="text"
+                    name="filter"
+                    value={filter}
+                    className={styles.input}
+                    onChange={onHandleChange}
+                />
+            </label>
+        </div>
+    );
+};
+
+export default Filter;
